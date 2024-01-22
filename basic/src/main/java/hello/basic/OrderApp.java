52685d4302f1f0a8e4ca687e1727d0375a0a69f3
focandlol <focandlol@naver.com>
2024-01-22 18:53:34 +0900

    section02 create ex

---
+++ basic/src/main/java/hello/basic/OrderApp.java
@@ -0,0 +1,22 @@
+package hello.basic;
+
+import hello.basic.member.Grade;
+import hello.basic.member.Member;
+import hello.basic.member.MemberService;
+import hello.basic.member.MemberServiceImpl;
+import hello.basic.order.Order;
+import hello.basic.order.OrderService;
+import hello.basic.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+    }
+}
---
+++ basic/src/test/java/hello/basic/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.basic.order;
+
+import hello.basic.member.Grade;
+import hello.basic.member.Member;
+import hello.basic.member.MemberService;
+import hello.basic.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
